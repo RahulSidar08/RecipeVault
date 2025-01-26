18c3f5ddf7162dd1bf9543f2f894a0f840de449c
RahulSidar08 <rahulsidar2056@gmail.com>
2025-01-27 01:38:34 +0530

    Project Creation

---
+++ frontend/Food-Recipe/src/pages/Home.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import foodRecipe from "../assets/foodRecipe.png";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import RecipeItems from "../components/RecipeItems";
+import { useNavigate } from "react-router-dom";
+import Modal from "../components/Modal";
+import InputForm from "../components/InputForm";
+import home1 from "../assets/home1.jpg";
+
+export default function Home() {
+  const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
+  console.log("Inside home components");
+
+  const addRecipe = () => {
+    let token = localStorage.getItem("token");
+    console.log(token);
+    if (token) navigate("/addRecipe");
+    else {
+      setIsOpen(true);
+    }
+  };
+
+  return (
+    <>
+      <section className="home">
+        <div className="left">
+          <h1>Food Recipe</h1>
+          <h5>
+            It is a long established fact that a reader will be distracted by
+            the readable content of a page when looking at its layout. The point
+            of using Lorem Ipsum is that it has a more-or-less normal
+            distribution of letters, as opposed to using 'Content here, content
+            here', making it look like readable English.
+          </h5>
+          <button onClick={addRecipe}>Create your recipe</button>
+        </div>
+        <div className="right">
+          <img src={foodRecipe}></img>
+          <img src={home1} />
+        </div>
+      </section>
+      <div className="bg">
+        {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#d4f6e8" fillOpacity="1" d="M0,32L40,32C80,32,160,32,240,58.7C320,85,400,139,480,149.3C560,160,640,128,720,101.3C800,75,880,53,960,80C1040,107,1120,181,1200,213.3C1280,245,1360,235,1400,229.3L1440,224L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path></svg> */}
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+          <path
+            fill="#a2d9ff"
+            fill-opacity="1"
+            d="M0,160L48,138.7C96,117,192,75,288,80C384,85,480,139,576,181.3C672,224,768,256,864,245.3C960,235,1056,181,1152,133.3C1248,85,1344,43,1392,21.3L1440,0L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+          ></path>
+        </svg>
+      </div>
+      {isOpen && (
+        <Modal onClose={() => setIsOpen(false)}>
+          <InputForm setIsOpen={() => setIsOpen(false)} />
+        </Modal>
+      )}
+      <div className="recipe">
+        <RecipeItems />
+      </div>
+    </>
+  );
+}
