cec3d5d7bcd99fe06d6d625803236b4097387e81
RahulSidar08 <rahulsidar2056@gmail.com>
2025-05-03 18:59:42 +0530

    added deployement links

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,24 @@
-const express=require("express")
-const app=express()
-const dotenv=require("dotenv").config()
-const connectDb=require("./config/connectionDb")
-const cors=require("cors")
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv").config();
+const connectDb = require("./config/connectionDb");
+const cors = require("cors");
 
-const PORT=process.env.PORT || 3000
-connectDb()
+const PORT = process.env.PORT || 3000;
+connectDb();
 
-app.use(express.json())
-app.use(cors())
-app.use(express.static("public"))
+app.use(express.json());
+const corsOptions = {
+  origin: "https://recipe-vault-navy.vercel.app",
+  credentials: true,
+};
+app.use(cors(corsOptions));
+app.use(cors());
+app.use(express.static("public"));
 
-app.use("/",require("./routes/user"))
-app.use("/recipe",require("./routes/recipe"))
+app.use("/", require("./routes/user"));
+app.use("/recipe", require("./routes/recipe"));
 
-app.listen(PORT,(err)=>{
-    console.log(`app is listening on port ${PORT}`)
-})
+app.listen(PORT, (err) => {
+  console.log(`app is listening on port ${PORT}`);
+});
